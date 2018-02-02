8ca3f111f2c8b781ced6addeade680da21de1422
Mark Seemann <mark@ploeh.dk>
2018-02-02 17:21:53 +0100

    Remove redundant Moq-related NuGet packages

--- CSharp/BookingApi.UnitTests/BookingApiTestConventionsAttribute.cs
+++ CSharp/BookingApi.UnitTests/BookingApiTestConventionsAttribute.cs
@@ -1,5 +1,4 @@
 ﻿using AutoFixture;
-using AutoFixture.AutoMoq;
 using AutoFixture.Xunit2;
 using System;
 
@@ -8,7 +7,7 @@ namespace Ploeh.Samples.BookingApi.UnitTests
     public class BookingApiTestConventionsAttribute : AutoDataAttribute
     {
         public BookingApiTestConventionsAttribute() :
-            base(() => new Fixture().Customize(new AutoMoqCustomization()))
+            base(() => new Fixture())
         {
         }
     }
