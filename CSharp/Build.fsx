99c5304e829a87b73c35021e49e3cba346334cee
Mark Seemann <mark@ploeh.dk>
2018-02-02 09:24:20 +0100

    Add test step to build script

--- CSharp/Build.fsx
+++ CSharp/Build.fsx
@@ -1,10 +1,18 @@
 ﻿#r @"packages/FAKE.4.64.4/tools/FakeLib.dll"
 
 open Fake
+open Fake.Testing
 
 Target "Build" <| fun _ ->
     !! "**/BookingApi.sln"
     |> MSBuildRelease "" "Rebuild"
     |> ignore
 
-RunTargetOrDefault "Build"
+Target "Test" <| fun _ ->
+    !! "*/bin/Release/Ploeh.Samples.*Tests.dll"
+    |> xUnit2 (fun p -> { p with Parallel = ParallelMode.All })
+
+"Build"
+==> "Test"
+
+RunTargetOrDefault "Test"
